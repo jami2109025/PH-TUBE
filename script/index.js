df4198cd963514a76e57a2ecaa4a74baad857f1e
jami-25 <jami25.bie01@gmail.com>
2025-03-28 02:48:27 +0600

    nav done

---
+++ script/index.js
@@ -0,0 +1,27 @@
+function loadCategories() {
+    // fetch data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        .then(response=>response.json())
+        .then(data=>displayCategories(data.categories));
+}
+
+function displayCategories(categories){
+    // get the container
+    const categoryContainer = document.getElementById("category-container");
+
+    // loop operation on Array of object
+    for(let category of categories){
+        console.log(category);
+        // create element
+        const categoryDiv = document.createElement('div');
+
+        categoryDiv.innerHTML = `
+            <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${category.category}</button>
+        `;
+
+        // Append the element
+        categoryContainer.append(categoryDiv);
+    }
+}
+
+loadCategories();
